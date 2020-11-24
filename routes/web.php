f5bc386f26a6fba1012c58230d0f912972cda4ba
keroles-star <59221288+keroles-star@users.noreply.github.com>
2020-11-24 14:18:06 +0200

    d

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::middleware('moderator')->group(function (){
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-});
 
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get("/reddit",[App\Http\Controllers\HomeController::class, 'reddit'])->name('reddit.search');
